8aae672836f9eba6044afed5de7ec625e5100f96
alex-js-ltd <alex.js.ltd@gmail.com>
2024-02-06 16:53:51 +0100

    update bubble sort

--- src/sort/bubble-sort.ts
+++ src/sort/bubble-sort.ts
@@ -1,16 +1,16 @@
 export const bubbleSort = (array: number[]) => {
-	let swapped = false
-	do {
-		swapped = false
-		for (let [index, value] of array.entries()) {
-			if (value > array[index + 1]) {
-				const temp = value
-				array[index] = array[index + 1]
-				array[index + 1] = temp
-				swapped = true
+	let sorted = false
+	while (!sorted) {
+		sorted = true
+
+		array.forEach((el, index, arr) => {
+			if (el > arr[index + 1]) {
+				arr[index] = arr[index + 1]
+				arr[index + 1] = el
+				sorted = false
 			}
-		}
-	} while (swapped)
+		})
+	}
 
 	return array
 }
